912e1c6b7d72cddcbe1654e264a79c10d975b902
chrialge <chrialge@gmail.com>
2024-02-20 16:20:44 +0100

    add problem with .value

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,52 @@
-console.log('hello');
+// console.log('hello');
 
-const priceTariff = 0.21;
+// const priceTariff = 0.21;
 
-/*
-    step1: chiedere kilometri e l'eta
-*/
+// /*
+//     step1: chiedere kilometri e l'eta
+// */
 
-const km = Number(prompt("dimmi quanti km vuoi fare?"));
-console.log(km);
+// const km = Number(prompt("dimmi quanti km vuoi fare?"));
+// console.log(km);
 
-let age = Number(prompt('dimmi la tua eta?'));
-console.log(age);
+// let age = Number(prompt('dimmi la tua eta?'));
+// console.log(age);
 
-/*
-    step 2: definire il prezzo senza nessun sconto
-*/
+// /*
+//     step 2: definire il prezzo senza nessun sconto
+// */
 
-let priceForKilometre = km * priceTariff;
-console.log(priceForKilometre);
-// variabili sconto
-const discountUnderAge = priceForKilometre/5;
-const discountOverAge = (priceForKilometre / 5) *2;
+// let priceForKilometre = km * priceTariff;
+// console.log(priceForKilometre);
+// // variabili sconto
+// const discountUnderAge = priceForKilometre/5;
+// const discountOverAge = (priceForKilometre / 5) *2;
 
-/*
-    step 3: applicare sconto in base l'eta
-*/
-let priceFinal
-if (age < 18) {
-    priceFinal = priceForKilometre - discountUnderAge;
-    console.log(priceFinal);
-} else if (age > 65){
-    priceFinal = priceForKilometre - discountOverAge;
-    console.log(priceFinal);
-}
+// /*
+//     step 3: applicare sconto in base l'eta
+// */
+// let priceFinal
+// if (age < 18) {
+//     priceFinal = priceForKilometre - discountUnderAge;
+//     console.log(priceFinal);
+// } else if (age > 65){
+//     priceFinal = priceForKilometre - discountOverAge;
+//     console.log(priceFinal);
+// }
 
-document.writeln(` questo e il costo del biglietto ${priceFinal.toFixed(2)}`);
-console.log(priceFinal)
+// document.writeln(` questo e il costo del biglietto ${priceFinal.toFixed(2)}`);
+// console.log(priceFinal)
 
 // input region
-const userKm = Number(document.getElementById("kilometre_user").value);
-const userAge = Number(document.getElementById("age_user").value);
+const userKm = Number(document.getElementById("kilometre_user"));
+const userAge = Number(document.getElementById("age_use"));
 let priceTrial
 
 const generate = document.querySelector("button")
 generate.addEventListener('click', 
 function () {
-    priceTrial = userKm * priceTariff;
+    priceTrial = Number(priceTrial.value);
+    priceTrial = userKm * (priceTariff);
     console.log(priceTrial);
-    
 }
 )
